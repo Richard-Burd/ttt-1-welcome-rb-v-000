6d2a0980cf8d54a1fdda146e2a93e3592682eb57
Richard Burd <richardburdx@yahoo.com>
2019-07-18 14:48:18 +0000

    Automatically backed up by Learn

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -5,3 +5,11 @@ describe "lib/welcome.rb" do
     load './lib/welcome.rb'
   end
 end
+
+describe "lib/welcome.rb" do
+  it 'prints "A second test"' do
+    expect($stdout).to receive(:puts).with("XP needs Test Driven Development"), "Make sure lib/welcome.rb has a second test that is working properly."
+    
+    load './lib/welcome.rb'
+  end
+end
