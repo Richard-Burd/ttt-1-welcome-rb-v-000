912bb9b2f15a859b303881eb7a164b7d8672860f
Richard Burd <richardburdx@yahoo.com>
2019-07-18 14:48:24 +0000

    Automatically backed up by Learn

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -1,7 +1,7 @@
 describe "lib/welcome.rb" do
   it 'prints "Welcome to Tic Tac Toe!"' do
     expect($stdout).to receive(:puts).with("Welcome to Tic Tac Toe!"), "Make sure lib/welcome.rb has code that can output 'Welcome to Tic Tac Toe!' exactly."
-    
+
     load './lib/welcome.rb'
   end
 end
@@ -9,7 +9,7 @@ end
 describe "lib/welcome.rb" do
   it 'prints "A second test"' do
     expect($stdout).to receive(:puts).with("XP needs Test Driven Development"), "Make sure lib/welcome.rb has a second test that is working properly."
-    
+
     load './lib/welcome.rb'
   end
 end
